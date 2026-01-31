f5fd2dcfdb186eefb7e833030e5152d70a492e31
Rupesh040899 <rupesh.mali0408@gmail.com>
2026-01-31 19:22:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./components/About/About";
 import Ecosystem from "./components/Ecosystem/Ecosystem";
 import FooterSection from "./components/FooterSection/FooterSection";
 import Home from "./components/Home/Home";
-import Products from "./components/products/products";
+import Products from "./components/products/Products";
 import Roadmap from "./components/Roadmap/Roadmap";
 
 export default function App() {
